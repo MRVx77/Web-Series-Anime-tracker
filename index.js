ca6bb532dd6bf4090bd6ea55d73716b8c18eddf1
mrv <mrv@gmail.com>
2025-11-13 23:05:35 +0530

    bug 1

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
-app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
